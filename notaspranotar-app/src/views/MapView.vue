6bb742a384345adb37ced4b5074ee32d26fc2fc7
Bárbara Boechat <barbs.boechat@gmail.com>
2022-06-26 20:02:17 -0300

    configuração da tela do mapa

--- notaspranotar-app/src/views/MapView.vue
+++ notaspranotar-app/src/views/MapView.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <NotasSection sectionId="header-liner">
+    <template v-slot:conteudo>
+      <div class="container-fluid text-center p-0">
+        <img
+          src="../assets/imagens/mapa.png"
+          alt="Mapa Pouso Alegre (MG) 2022"
+          style="width: 100%"
+        />
+      </div>
+    </template>
+  </NotasSection>
 </template>
 
-<style></style>
+<style lang="sass" scoped>
+@import "../assets/styles/variaveis"
+
+#header-liner
+    padding: 5rem 0 0 !important
+
+@media (max-width: $md)
+    #header-liner
+      padding: 9rem 0 0 !important
+</style>
